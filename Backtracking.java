648b259aaa22c64850bd54f594bf30b15c5f233f
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-04-27 21:40:50 +0530

    hi

--- Backtracking.java
+++ Backtracking.java
@@ -19,8 +19,8 @@ public class Backtracking {
         System.out.println();
     }
 
-    public static void FindSubsets(String str, int i, String ans) {  // Find the  possible subsets of the string
-        if (i == str.length()) {
+    public static void FindSubsets(String str, int i, String ans) { // Find the possible subsets of the string
+        if (i == str.length()) { // O(2^n *n) time and O(n) space
             if (ans.length() == 0) {
                 System.out.println("null");
             } else {
@@ -32,15 +32,84 @@ public class Backtracking {
         FindSubsets(str, i + 1, ans);
     }
 
+    public static void PermutationsOfString(String str, String ans) {// find all the permutations of a string
+        if (str.length() == 0) { // O(n*n!) -time
+            System.out.println(ans);
+            return;
+        }
 
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String Newstr = str.substring(0, i) + str.substring(i + 1);
+            PermutationsOfString(Newstr, ans + curr);
+        }
+    }
 
+    public static boolean isSafe(char board[][],int row,int col){
+        for(int i = row-1;i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        
+        for(int i = row-1,j = col-1;i >=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void NQueen(char board[][], int row) {
+        if (row == board.length) {
+            printBoard(board);
+            return;
+
+        }
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                NQueen(board, row + 1);
+                board[row][j] = '.';
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("---------chess board-----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[5];
         changeArr(arr, 0, 1);
         printArr(arr);
         String str = "abc";
-        FindSubsets(str, 0, "");
+        // FindSubsets(str, 0, "");
+        PermutationsOfString(str, "");
+        int n = 4;
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        NQueen(board, 0);
 
     }
 }
