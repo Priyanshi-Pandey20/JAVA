31cf2006f4ba1e0863774978641a5d11adfe456a
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-05-23 20:16:36 +0530

    hy

--- AVLTree.java
+++ AVLTree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class AVLTree {
     static class Node {
         int data, height;
@@ -20,16 +21,14 @@ public class AVLTree {
 
     }
 
-   
-
     public static Node rightRotate(Node y) {
         Node x = y.left;
         Node T2 = x.right;
 
         x.right = y;
         y.left = T2;
-        y.height =Math. max(height(y.left), height(y.right)) + 1;
-        x.height =Math. max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
 
         return x;
     }
@@ -41,8 +40,8 @@ public class AVLTree {
         y.left = x;
         x.right = T2;
 
-        x.height =Math.max(height(x.left), height(x.right)) + 1;
-        y.height =Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
         return y;
     }
 
@@ -53,21 +52,19 @@ public class AVLTree {
         return height(root.left) - height(root.right);
     }
 
-    public static Node insert(Node root, int key) {
+    public static Node insert(Node root, int key) { // insertion
         if (root == null) {
             return new Node(key);
         }
         if (key < root.data) {
             root.left = insert(root.left, key);
-        }
-         else if (key > root.data) {
+        } else if (key > root.data) {
             root.right = insert(root.right, key);
-        } 
-        else {
+        } else {
             return root;
         }
         root.height = 1 + Math.max(height(root.left), height(root.right));
-        
+
         int bf = getBalance(root);
 
         if (bf > 1 && key < root.left.data) { // LL case
@@ -97,13 +94,85 @@ public class AVLTree {
         preorder(root.right);
     }
 
+    public static Node getMinNode(Node root) { // deletion 
+        Node curr = root;
+
+        while (curr.left != null)
+            curr = curr.left;
+        return curr;
+
+    }
+
+    public static Node deleteNode(Node root, int key) {
+
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = deleteNode(root.left, key);
+        }
+
+        else if (key > root.data) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null || root.right == null) {
+                Node temp = null;
+                if (temp == root.left) {
+                    temp = root.right;
+                } else
+                    temp = root.left;
+
+                if (temp == null) {
+                    temp = root;
+                    root = null;
+                } else
+                    root = temp;
+
+            } else {
+                Node temp = getMinNode(root.left);
+                root.data = temp.data;
+                root.right = deleteNode(root.right, key);
+            }
+
+        }
+        if (root == null)
+            return root;
+
+        root.height = Math.max(height(root.left), height(root.right)) + 1;
+
+        int bf = getBalance(root);
+
+        if (bf > 1 && getBalance(root.left) >= 0)
+            return rightRotate(root);
+
+        if (bf > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+        if (bf < -1 && getBalance(root.right) <= 0)
+            return leftRotate(root);
+
+        if (bf < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
+
+    }
+
     public static void main(String[] args) {
         root = insert(root, 10);
         root = insert(root, 20);
         root = insert(root, 30);
         root = insert(root, 40);
-        root = insert(root, 50);
-        root = insert(root, 25);
+        root = insert(root,50);
+         root = insert(root,25);
+        
+        root = deleteNode(root, 50);
+      
+        
 
         preorder(root);
 
