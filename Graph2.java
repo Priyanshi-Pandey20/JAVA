9b8e2e2f811e8e8715f705a923b67f5675245598
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-13 11:56:42 +0530

    graphs

--- Graph2.java
+++ Graph2.java
@@ -1,55 +1,112 @@
-import java .util.*;
+import java.util.*;
+
 public class Graph2 {
-     static class Edge {
+    static class Edge {
         int src;
         int dest;
-    
-        public Edge(int s, int d) {
+        int wt;
+
+        public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
+            this.wt = w;
 
         }
     }
-     static void createGraph(ArrayList<Edge> graph[]) { // create a graph
+
+    static void createGraph(ArrayList<Edge> graph[]) { // create a graph
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
 
         }
-        graph[0].add(new Edge(0, 3));
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
 
-        graph[2].add(new Edge(2, 3));
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
 
-        graph[3].add(new Edge(3, 1));
+        graph[2].add(new Edge(2, 4, 3));
 
-        graph[4].add(new Edge(4, 0));
-        graph[4].add(new Edge(4, 1));
+        graph[3].add(new Edge(3, 5, 1));
 
-
-        graph[5].add(new Edge(5, 0));
-        graph[5].add(new Edge(5, 2));
-
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
 
     }
-    
-    public static void printAllPaths(ArrayList<Edge>[] graph,int src,int dest,String path){ // print all paths from source to target
-        if(src == dest){
-            System.out.println(path+dest);
+
+    public static void printAllPaths(ArrayList<Edge>[] graph, int src, int dest, String path) { // print all paths from
+                                                                                                // source to target
+        if (src == dest) {
+            System.out.println(path + dest);
             return;
         }
-        for(int i =0;i<graph[src].size();i++){
+        for (int i = 0; i < graph[src].size(); i++) {
             Edge e = graph[src].get(i);
             printAllPaths(graph, e.dest, dest, path + src);
         }
     }
 
-    public static void main(String[] args) {
-          int V = 6;
-        ArrayList<Edge>[] graph = new ArrayList[V];
+    static class Pair implements Comparable<Pair> { // Dijkstra Algorithm
+        int n;
+        int path;
 
+        public Pair(int n, int path) {
+            this.n = n;
+            this.path = path;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
+        }
+    }
+
+    public static void dijkstra(ArrayList<Edge> graph[], int src) {
+        int dist[] = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    Edge e = graph[curr.n].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if (dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
+    }
+
+    
+
+
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        int src = 5;
-        int dest = 1;
-        printAllPaths(graph, src, dest, "");
-        
+
+        int src = 0;
+        dijkstra(graph, src);
+
     }
 }
