b4aae31c8caed486c5e3d15765fbb2e0187c8d2e
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-14 23:54:36 +0530

    graph

--- Graph2.java
+++ Graph2.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class Graph2 {
     static class Edge {
@@ -14,23 +15,18 @@ public class Graph2 {
         }
     }
 
-    static void createGraph(ArrayList<Edge> graph[]) { // create a graph
+    static void createGraph(int[][] flights, ArrayList<Edge> graph[]) { // create a graph
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
 
         }
-        graph[0].add(new Edge(0, 1, 10));
-        graph[0].add(new Edge(0, 2, 15));
-        graph[0].add(new Edge(0, 3, 30));
-
-        graph[1].add(new Edge(1, 0, 10));
-        graph[1].add(new Edge(1, 3, 40));
-
-        graph[2].add(new Edge(2, 0, 15));
-        graph[2].add(new Edge(2, 3, 50));
-
-        graph[3].add(new Edge(3, 1, 40));
-        graph[3].add(new Edge(3, 2, 50));
+        for (int i = 0; i < flights.length; i++) {
+            int src = flights[i][0];
+            int dest = flights[i][1];
+            int wt = flights[i][2];
+            Edge e = new Edge(src, dest, wt);
+            graph[src].add(e);
+        }
 
     }
 
@@ -127,7 +123,7 @@ public class Graph2 {
         System.out.println();
     }
 
-    static class Pair1 implements Comparable<Pair1> {  // Prims Algorithm
+    static class Pair1 implements Comparable<Pair1> { // Prims Algorithm
         int v;
         int cost;
 
@@ -168,15 +164,67 @@ public class Graph2 {
 
     }
 
-    
+    static class Info {
+        int v;
+        int cost;
+        int stops;
 
+        public Info(int v, int c, int s) {
+            this.v = v;
+            this.cost = c;
+            this.stops = s;
 
+        }
+
+    }
+
+    public static int cheapestFlight(int n, int[][] flights, int src, int dest, int k) {
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        createGraph(flights, graph);
+
+        int[] dist = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        Queue<Info> q = new LinkedList<>();
+        q.add(new Info(src, 0, 0));
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+
+            if (curr.stops > k) {
+                break;
+            }
+            for (int i = 0; i < graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+                int u = e.src;
+                int v = e.dest;
+                int wt = e.wt;
+
+                if (curr.cost + wt < dist[v] && curr.stops <= k) {
+                    dist[v] = curr.cost + wt;
+                    q.add(new Info(v, dist[v], curr.stops + 1));
+                }
+            }
+        }
+        if (dist[dest] == Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return dist[dest];
+        }
+
+    }
 
     public static void main(String[] args) {
-        int V = 4;
-        ArrayList<Edge>[] graph = new ArrayList[V];
-        createGraph(graph);
-        prims(graph);
+        int n = 4;
+        ArrayList<Edge>[] graph = new ArrayList[n];
 
+        int[][] flights = { { 0, 1, 100 }, { 1, 2, 100 }, { 2, 0, 100 }, { 1, 3, 600 }, { 2, 3, 200 } };
+        int src = 0;
+        int dest = 3;
+        int k = 1;
+        System.out.println(cheapestFlight(n, flights, src, dest, k));
     }
 }
