fa2dadd6392e804fd460171cf0ba5e770f6ec313
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-18 19:40:31 +0530

    graph3

--- Graph3.java
+++ Graph3.java
@@ -30,13 +30,8 @@ public class Graph3 {
 
         graph[3].add(new Edge(3, 0));
         graph[3].add(new Edge(3, 4));
-        graph[3].add(new Edge(3, 5));
 
         graph[4].add(new Edge(4, 3));
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 3));
-        graph[5].add(new Edge(5, 4));
 
     }
 
@@ -135,15 +130,58 @@ public class Graph3 {
 
     }
 
-    
+    public static void dfs1(ArrayList<Edge>[] graph, int curr, int par, int dt[], int low[], boolean[] vis, int time,   // find articulation points using tarjan's
+            boolean[] ap) {
 
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;
+        int children = 0;
 
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            int neigh = e.dest;
 
+            if (par == neigh) {
+                continue;
+            } else if (vis[neigh]) {
+                low[curr] = Math.min(low[curr], dt[neigh]);
+            } else {
+                dfs1(graph, neigh, curr, dt, low, vis, time, ap);
+                low[curr] = Math.min(low[curr], low[neigh]);
+                if (par != -1 && dt[curr] <= low[neigh]) {
+                    ap[curr] = true;
+                }
+                children++;
+            }
+        }
+        if (par == -1 && children > 1) {
+            ap[curr] = true;
+        }
+    }
 
+    public static void getAP(ArrayList<Edge>[] graph, int V) {
+        int[] dt = new int[V];
+        int[] low = new int[V];
 
+        int time = 0;
+        boolean[] vis = new boolean[V];
+        boolean[] ap = new boolean[V];
 
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs1(graph, i, -1, dt, low, vis, time, ap);
+            }
+        }
 
+        for (int i = 0; i < V; i++) {
+            if (ap[i]) {
+                System.out.println("AP : " + i);
+            }
+        }
 
+    }
+
+     
 
     public static void main(String[] args) {
         int V = 6;
@@ -151,5 +189,6 @@ public class Graph3 {
         createGraph(graph);
         kosaraju(graph, V);
         tarjanBridge(graph, V);
+        getAP(graph, V);
     }
 }
