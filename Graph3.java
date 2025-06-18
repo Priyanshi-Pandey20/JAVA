8e63af7b78de0a3a5f0d25b4c841e1c80ca649c5
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-18 11:02:55 +0530

    graph3

--- Graph3.java
+++ Graph3.java
@@ -18,18 +18,30 @@ public class Graph3 {
             graph[i] = new ArrayList<>();
         }
 
+        graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
         graph[0].add(new Edge(0, 3));
 
         graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
 
         graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 2));
 
+        graph[3].add(new Edge(3, 0));
         graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
 
     }
 
-    public static void topSort(ArrayList<Edge>[] graph, int curr, boolean[] vis, Stack<Integer> s) {  //Kosaraju Algorithm
+    public static void topSort(ArrayList<Edge>[] graph, int curr, boolean[] vis, Stack<Integer> s) { // Kosaraju
+                                                                                                     // Algorithm
         vis[curr] = true;
 
         for (int i = 0; i < graph[curr].size(); i++) {
@@ -78,14 +90,14 @@ public class Graph3 {
         while (!st.isEmpty()) {
             int curr = st.pop();
             if (!vis[curr]) {
-                dfs(transpose,curr,vis);
-                
+                dfs(transpose, curr, vis);
+
             }
         }
 
     }
 
-
+    
 
     public static void main(String[] args) {
         int V = 5;
