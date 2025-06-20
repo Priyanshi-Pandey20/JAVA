07bd4368e41b99779968d2ba7411d8a803fa082b
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-20 21:08:24 +0530

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -107,23 +107,33 @@ public class DynamicProgramming {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        int n = 5;
-        int[] ways = new int[n + 1];
-        Arrays.fill(ways, -1);
-        // System.out.println(climbStairsTab(n));
+    public static boolean targetSum(int[] arr, int sum) { // target sum
+        int n = arr.length;
+        boolean[][] dp = new boolean[n + 1][sum+1];
 
-        int[] val = { 15, 14, 10, 45, 30 };
-        int[] wt = { 2, 5, 1, 3, 4 };
-        int W = 7;
-        int dp[][] = new int[val.length + 1][W + 1];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] = -1;
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                int v = arr[i - 1];
+
+                if (v <= j && dp[i - 1][j - v] == true) {
+                    dp[i][j] = true;
+                } else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
             }
         }
-        print(dp);
-        System.out.println(knapsackTab(wt, val, W));
+        return dp[n][sum];
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 2, 7, 1, 3 };
+        int sum = 10;
+        System.out.println(targetSum(arr, sum));
 
     }
 }
