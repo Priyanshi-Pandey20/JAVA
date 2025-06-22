518bca90d4be960179607a63c42782a6b997cf5d
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-22 21:03:51 +0530

    function

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,15 @@
-dp
+ public static void Palindrome(int n) {// palindrome
+        int original = n;
+        int rev = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+        if (original == rev) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("not palindrome");
+        }
+
+    }
