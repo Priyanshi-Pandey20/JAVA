70a6d4d0d89864af93d15fc70dd96eb5b1d492ee
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-23 19:18:17 +0530

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -175,7 +175,7 @@ public class DynamicProgramming {
         return dp[n][sum];
     }
 
-    public static int rodCutting(int[] length, int[] price, int totRod) {
+    public static int rodCutting(int[] length, int[] price, int totRod) { // rod cutting problem
         int n = price.length;
         int[][] dp = new int[n + 1][totRod + 1];
 
@@ -199,11 +199,45 @@ public class DynamicProgramming {
         return dp[n][totRod];
     }
 
+    public static int longestCommonSubsequence(String str1,String str2,int n,int m ,int dp[][]){ // lcs using memoization
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
+
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+           return dp[n][m] = longestCommonSubsequence(str1, str2, n-1, m,dp)+1;
+
+        }
+        else{
+            int ans1 = longestCommonSubsequence(str1, str2, n-1, m, dp);
+            int ans2 = longestCommonSubsequence(str1, str2, n, m-1, dp);
+            return dp[n][m] = Math.max(ans1,ans2);
+        }
+  }
+
+  
+
+
+
+
+
     public static void main(String[] args) {
-        int[] length = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int[] price = { 1, 5, 8, 9, 10, 17, 17, 20 };
-        int totRod = 8;
-        System.out.println(rodCutting(length, price, totRod));
+       String str1 = "abcdge";
+       String str2 = "abedg";
+       int n = str1.length();
+       int m = str2.length();
+       int[][] dp = new int[n+1][m+1];
+
+       for(int i =0;i<n+1;i++){
+        for(int j =0;j<m+1;j++){
+            dp[i][j]= -1;
+        }
+       }
+       System.out.println(longestCommonSubsequence(str1, str2, n, m, dp));
+
 
     }
 }
