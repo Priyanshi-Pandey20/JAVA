c923d8f17fa6d5ad106bd9479ee8114188965b0e
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-23 22:24:33 +0530

    segment tree

--- SegmentTrees.java
+++ SegmentTrees.java
@@ -1,35 +1,33 @@
 public class SegmentTrees {
-    static int Cst(int ss,int se,int si,int[] arr,int[] tree){
-        if(ss== se){
+    static int Cst(int ss, int se, int si, int[] arr, int[] tree) {
+        if (ss == se) {
             tree[si] = arr[ss];
             return tree[si];
 
         }
-        int mid = (ss+se)/2;
-        tree[si]= Cst(ss, mid, 2*si+1, arr, tree) + Cst(mid+1, se, 2* si+2, arr, tree);
+        int mid = (ss + se) / 2;
+        tree[si] = Cst(ss, mid, 2 * si + 1, arr, tree) + Cst(mid + 1, se, 2 * si + 2, arr, tree);
         return tree[si];
     }
-    
-    static int getSumRec(int qs,int qe,int ss,int se,int si,int[] tree){
-        if(se<qs || ss> qe){
+
+    static int getSumRec(int qs, int qe, int ss, int se, int si, int[] tree) {
+        if (se < qs || ss > qe) {
             return 0;
         }
-        if(qs<=ss && qe >= se){
+        if (qs <= ss && qe >= se) {
             return tree[si];
         }
-        int mid = (ss+se)/2;
+        int mid = (ss + se) / 2;
 
-        return getSumRec(qs, qe, ss, mid, 2*si+1, tree)+ getSumRec(qs, qe, mid+1, se, 2*si+2, tree);
+        return getSumRec(qs, qe, ss, mid, 2 * si + 1, tree) + getSumRec(qs, qe, mid + 1, se, 2 * si + 2, tree);
     }
-    public static void main(String[] args) {
-        int[] arr = {10,20,30,40};
-       int n = 4;
-        int[] tree = new int[4*n];
-        System.out.println(Cst(0, n-1, 0, arr, tree));
-        System.out.println(getSumRec(n, n, n, n, n, tree));
 
-        for(int i =0;i<4*n;i++){
-            System.out.println(tree[i]);
-        }
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40 };
+        int n = 4;
+        int[] tree = new int[4 * n];
+        System.out.println(Cst(0, n - 1, 0, arr, tree));
+
+       System.out.println(getSumRec(0, 2, 0, 3, 0, tree));
     }
 }
