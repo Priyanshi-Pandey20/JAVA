c4b5b9a0805e4adc12c1990744426c32ca6d9ecb
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-23 21:20:53 +0530

    custom array

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -198,8 +198,9 @@ public class DynamicProgramming {
         return dp[n][totRod];
     }
 
-    public static int longestCommonSubsequence(String str1, String str2, int n, int m, int dp[][]) { // lcs using memoization
-                                                                                                    
+    public static int longestCommonSubsequence(String str1, String str2, int n, int m, int dp[][]) { // lcs using
+                                                                                                     // memoization
+
         if (n == 0 || m == 0) {
             return 0;
         }
@@ -244,37 +245,31 @@ public class DynamicProgramming {
         return dp[n][m];
     }
 
-    public static int longestCommonSubstring(String str1,String str2){ // longest common substring
+    public static int longestCommonSubstring(String str1, String str2) { // longest common substring
         int n = str1.length();
         int m = str2.length();
-        int dp[][] = new int[n+1][m+1];
+        int dp[][] = new int[n + 1][m + 1];
         int ans = 0;
 
-        for(int i =0;i<n+1;i++){
+        for (int i = 0; i < n + 1; i++) {
             dp[i][0] = 0;
         }
-        for(int j =0;j<m+1;j++){
+        for (int j = 0; j < m + 1; j++) {
             dp[0][j] = 0;
         }
-        for(int i =1;i<n+1;i++){
-            for(int j = 1;j<m+1;j++){
-                if(str1.charAt(i-1) == str2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                   ans = Math.max(ans, dp[i][j]);
-                }
-                else{
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
                     dp[i][j] = 0;
                 }
             }
         }
         return ans;
 
-
-
-
     }
-
-
 
     public static void main(String[] args) {
         String str1 = "ABCDE";
@@ -284,9 +279,9 @@ public class DynamicProgramming {
         int[][] dp = new int[n + 1][m + 1];
 
         // for (int i = 0; i < n + 1; i++) {
-        //     for (int j = 0; j < m + 1; j++) {
-        //         dp[i][j] = -1;
-        //     }
+        // for (int j = 0; j < m + 1; j++) {
+        // dp[i][j] = -1;
+        // }
         // }
         System.out.println(longestCommonSubstring(str1, str2));
     }
