e0414fcf3994abeb539800a71f0ca1bda5713109
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-24 18:50:05 +0530

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -218,7 +218,7 @@ public class DynamicProgramming {
         }
     }
 
-    public static int longestCommonSubsequence2(String str1, String str2) { // lcs using
+    public static int longestCommonSubsequence2(String str1, String str2) { // lcs using tabulation
         int n = str1.length();
         int m = str2.length();
 
@@ -271,6 +271,49 @@ public class DynamicProgramming {
 
     }
 
+    public static int lcs(int[] arr1,int[] arr2){
+        int n = arr1.length;
+        int m = arr2.length;
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i =0;i<n+1;i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j<m+1;j++){
+            dp[0][j]= 0;
+        }
+
+        for(int i =1;i<n+1;i++){
+            for(int j =1;j<m+1;j++){
+              if(arr1[i-1] == arr2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1;
+              }
+              else{
+                int ans1 = dp[i-1][j];
+                int ans2 = dp[i][j-1]+1;
+                dp[i][j]= Math.max(ans1,ans2);
+              }
+            }
+        }
+        return dp[n][m];
+    }
+      
+    public static int LIS(int[] arr1){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+        int[] arr2 = new int[set.size()];
+        int i =0;
+        for(int num : set){
+            arr2[i] = num;
+            i++;
+        }
+        Arrays.sort(arr2);
+      return lcs(arr1,arr2);
+    }
+
     public static void main(String[] args) {
         String str1 = "ABCDE";
         String str2 = "ABGCE";
@@ -278,11 +321,8 @@ public class DynamicProgramming {
         int m = str2.length();
         int[][] dp = new int[n + 1][m + 1];
 
-        // for (int i = 0; i < n + 1; i++) {
-        // for (int j = 0; j < m + 1; j++) {
-        // dp[i][j] = -1;
-        // }
-        // }
-        System.out.println(longestCommonSubstring(str1, str2));
+
+      int[] arr = {50,3,10,7,40,80};
+        System.out.println(LIS(arr));
     }
 }
