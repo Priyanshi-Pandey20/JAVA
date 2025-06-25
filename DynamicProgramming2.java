76c73b3f31dddc912261e87a65b45dbe90e9e6a9
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-25 19:40:10 +0530

    dp2

--- DynamicProgramming2.java
+++ DynamicProgramming2.java
@@ -34,11 +34,22 @@ public class DynamicProgramming2 {
 
     }
 
-    
+    public static int catalnRec(int n){
+        if(n == 0 || n==1){
+            return 1;
+        }
+        int ans = 0;
+        for(int i =0;i<=n-1;i++){
+            ans+=catalnRec(i) *catalnRec(n-i-1);
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         String s = "baaabab";
         String p = "*****ba*****ab";
         System.out.println(wildcardMatching(s, p));
+        int n =4;
+       System.out.println(catalnRec(n));
     }
 }
