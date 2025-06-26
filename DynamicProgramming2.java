e919e3939df90e05d7c22cc87e7ce16e13adebb1
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-26 19:59:44 +0530

    dp2

--- DynamicProgramming2.java
+++ DynamicProgramming2.java
@@ -106,9 +106,9 @@ public class DynamicProgramming2 {
             return 0;
         }
         int ans = Integer.MAX_VALUE;
-        for (int k = i; k < j - 1; k++) {
+        for (int k = i; k <= j - 1; k++) {
             int cost1 = mcm(arr, i, k);
-            int cost2 = mcm(arr, i + 1, j);
+            int cost2 = mcm(arr, k + 1, j);
             int cost3 = arr[i - 1] * arr[k] * arr[j];
             int finalCost = cost1 + cost2 + cost3;
             ans = Math.min(ans, finalCost);
@@ -117,8 +117,9 @@ public class DynamicProgramming2 {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr = { 1, 2, 3, 4, 3 };
         int n = arr.length;
-        System.out.println(mcm(arr, n, n));
+        
+        System.out.println(mcm(arr, 1, n-1));
     }
 }
