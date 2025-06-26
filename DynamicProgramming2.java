c0893f2e620a46cffe8e010752929c6c61aa16ca
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-26 20:20:38 +0530

    dp2

--- DynamicProgramming2.java
+++ DynamicProgramming2.java
@@ -116,10 +116,34 @@ public class DynamicProgramming2 {
         return ans;
     }
 
+    public static int mcmMem(int[] arr, int i, int j, int[][] dp) { // matrix chain multiplication using memoization
+
+        if (i == j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcm(arr, i, k);
+            int cost2 = mcm(arr, k + 1, j);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+            dp[i][j] = ans;
+        }
+        return dp[i][j];
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 3 };
         int n = arr.length;
-        
-        System.out.println(mcm(arr, 1, n-1));
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.println(mcmMem(arr, 1, n - 1,dp));
     }
 }
