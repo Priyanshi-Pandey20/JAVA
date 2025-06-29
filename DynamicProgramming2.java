24596f149bfdc819ac10c03ee70ed7cc5cd78217
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-06-29 19:28:06 +0530

    dp2

--- DynamicProgramming2.java
+++ DynamicProgramming2.java
@@ -181,13 +181,37 @@ public class DynamicProgramming2 {
 
         int sum1 = dp[n][W];
         int sum2 = sum - sum1;
-        return Math.abs(sum1-sum2);
+        return Math.abs(sum1 - sum2);
+
+    }
+
+    public static int minarrayJumps(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            int steps = arr[i];
+            int ans = Integer.MAX_VALUE;
+            for (int j = i + 1; j <= i + steps; j++) {
+               if(dp[j] != -1){
+                ans = Math.min(ans,dp[j]+1);
+               }
+            }
+            if(ans != Integer.MAX_VALUE){
+                dp[i] = ans;
+            }
+            
+        }
+        return dp[0];
+        
 
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 6, 11, 5 };
-        System.out.println(minimumPartition(nums));
+        int[] arr = { 2, 3, 1, 1, 4 };
+        System.out.println(minarrayJumps(arr));
 
     }
 }
