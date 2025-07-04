25163a5d2b4cd341894fd805a0f06cb679a686ac
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-07-04 21:08:12 +0530

    two d array

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.Arrays;
+import java .util.ArrayList;
 public class TwoDArray {
     
 
@@ -35,7 +36,7 @@ public class TwoDArray {
         }
         System.out.println(max);
 
-        int[][] c = { { 7, 8, 9 }, { 2, 3, 1 } }; // min element in 2d array
+        int[][] c = { { 7, 8, 9 }, { 2, 3, 1 } }; // min element in 2d array 
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < c[i].length; j++) {
