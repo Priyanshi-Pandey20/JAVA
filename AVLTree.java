605d51bc2a500e3f05ca8953fb58f7607d3f5ac5
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-07-10 22:08:49 +0530

    avl

--- AVLTree.java
+++ AVLTree.java
@@ -21,7 +21,7 @@ public class AVLTree {
 
     }
 
-    public static Node rightRotate(Node y) {
+    public static Node rightRotate(Node y) { // right rotate
         Node x = y.left;
         Node T2 = x.right;
 
