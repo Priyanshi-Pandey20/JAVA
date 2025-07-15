2068f86ab70d61c780e19b29641cd074570c5659
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-07-15 21:13:52 +0530

    practise

--- Practise1.java
+++ Practise1.java
@@ -68,7 +68,7 @@ public class Practise1 {
                     System.out.println(table);
                 }
 
-                int k = scn.nextInt(); // Q8
+                int k = scn.nextInt(); // Q8 // check whether the given  no. is divisble or not 
 
                 if (k % 5 == 0 && k % 11 == 0) {
                     System.out.println("It is divisible by 5 and 11");
