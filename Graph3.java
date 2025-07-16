caea9568be0c220a83086bf070352327cdfd262a
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-07-16 22:27:14 +0530

    graph

--- Graph3.java
+++ Graph3.java
@@ -60,7 +60,7 @@ public class Graph3 {
 
     }
 
-    public static void kosaraju(ArrayList<Edge>[] graph, int V) {
+    public static void kosaraju(ArrayList<Edge>[] graph, int V) { // kosaraju algorithm
         Stack<Integer> st = new Stack<>();
         boolean[] vis = new boolean[V];
 
