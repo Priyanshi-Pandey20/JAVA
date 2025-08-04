bc787508e11f700f1d208aa58399662feaefc88c
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-08-04 13:22:43 +0530

    segment tree

--- SegmentTrees.java
+++ SegmentTrees.java
@@ -74,35 +74,42 @@ public class SegmentTrees {
         return getSumUtil(0, 0, n - 1, qi, qj);
     }
 
-     public static void buildTree(int[] arr, int i, int start, int end) { // build segment tree
-        if (start == end) {
-            tree[i] = arr[start];
+     public static void buildTree(int i, int si, int sj, int[] arr) { // build segment tree to find max no.
+        if (si == sj) {
+            tree[i] = arr[si];
             return ;
         }
 
-        int mid = (start + end) / 2;
-        buildST(arr, 2 * i + 1, start, mid);
-        buildST(arr, 2 * i + 2, mid + 1, end);
+        int mid = (si + sj) / 2;
+        buildTree(2*i+1,si,mid,arr);
+        buildTree(2*i+2,mid+1,sj,arr);
         tree[i] = Math.max(tree[2 * i + 1] , tree[2 * i + 2]);
-        
-
     }
+
+    
+
    
 
     public static void main(String[] args) {
 
         int[] arr = { 6, 8, -1, 2, 17, 1, 3, 2, 4 };
+
         int n = arr.length;
         init(n);
-        System.out.println(buildST(arr, 0, 0, n - 1));
+        buildTree(0, 0, n-1, arr);
+        for(int i = 0;i<tree.length;i++){
+            System.out.print(tree[i] + " ");
+        }
 
-        int[] arr1 = { 10, 20, 30, 40 };
-        int n2 = 4;
-        int[] tree = new int[4 * n];
-        System.out.println(Cst(0, n - 1, 0, arr, tree));
+      
 
-        System.out.println(getSumRec(0, 2, 0, 3, 0, tree));
+        // int[] arr1 = { 10, 20, 30, 40 };
+        // int n2 = 4;
+        // int[] tree = new int[4 * n];
+        // System.out.println(Cst(0, n - 1, 0, arr, tree));
 
-        System.out.println(getSum(arr, 2, 5));
+        // System.out.println(getSumRec(0, 2, 0, 3, 0, tree));
+
+        // System.out.println(getSum(arr, 2, 5));
     }
 }
