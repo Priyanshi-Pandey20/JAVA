0992467a1193ec0a70f5a98761377d911964ca1d
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-08-04 13:36:47 +0530

    segement tree

--- SegmentTrees.java
+++ SegmentTrees.java
@@ -86,7 +86,25 @@ public class SegmentTrees {
         tree[i] = Math.max(tree[2 * i + 1] , tree[2 * i + 2]);
     }
 
-    
+    public static int getMax(int[] arr,int qi,int qj){
+        int n = arr.length;
+        return getMaxUtil(0,0,n-1,qi,qj);
+    }
+
+    public static int getMaxUtil(int i ,int si,int sj,int qi,int qj){
+         if(si > qj || sj<qi){
+            return Integer.MIN_VALUE;
+         }
+         else if(si>=qi && sj<= qj){
+            return tree[i];
+         }
+         else{
+            int mid = (si+sj)/2;
+            int leftAns = getMaxUtil(2*i+1, si, mid,qi,qj);
+            int rightAns = getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+            return Math.max(leftAns,rightAns);
+         }
+    }
 
    
 
