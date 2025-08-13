abe5d14cd645274195e642f4a5020dec19a23981
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-08-13 12:50:59 +0530

    graph

--- Graph2.java
+++ Graph2.java
@@ -264,7 +264,7 @@ public class Graph2 {
     static int[] par = new int[n];
     static int[] rank = new int[n];
 
-    public static void init() {
+    public static void init() { // init 
         for (int i = 0; i < n; i++) {
             par[i] = i;
         }
