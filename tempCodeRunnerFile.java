845dd296dd11b4251555246cdec99cace442a459
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-08-16 16:44:49 +0530

    array

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,17 @@
-n
+public static void SortArrayInAsending(int[] arr1) { //sort 
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = i + 1; j < arr1.length; j++) {
+                if (arr1[i] > arr1[j]) {
+                    int temp = arr1[i];
+                    arr1[i] = arr1[j];
+                    arr1[j] = temp;
+                }
+            }
+
+        }
+        for (int j = 0; j < arr1.length; j++) {
+            System.out.println(arr1[j]);
+        }
+
+    }
