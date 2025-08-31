49bc1e437434e8644a6f8080edf2a80e7ef088aa
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-08-31 21:21:41 +0530

    avl

--- AVLTree.java
+++ AVLTree.java
@@ -33,7 +33,7 @@ public class AVLTree {
         return x;
     }
 
-    public static Node leftRotate(Node x) { // left rotate
+    public static Node leftRotate(Node x) { // left rotate function 
         Node y = x.right;
         Node T2 = y.left;
 
