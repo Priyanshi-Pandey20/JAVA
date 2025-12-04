5794f8f0619af83df60285d669af22dbbb6bec06
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2025-12-04 21:32:38 +0530

    array

--- DynamicProgramming2.java
+++ DynamicProgramming2.java
@@ -212,7 +212,7 @@ public class DynamicProgramming2 {
 
     public static void main(String[] args) {
         int[] arr = { 2, 3, 1, 1, 4 };
-        System.out.println(minarrayJumps(arr));
+        //System.out.println(minarrayJumps(arr));
 
     }
 }
