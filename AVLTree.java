b735cc1f5b331597507bff2c8aecea26a9c88dc0
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-06-28 19:33:25 +0530

    hi

--- AVLTree.java
+++ AVLTree.java
@@ -85,7 +85,7 @@ public class AVLTree {
 
     }
 
-    public static void preorder(Node root) { // preorder in avl 
+    public static void preorder(Node root) { // preorder in avl  pblm 
         if (root == null) {
             return;
         }
