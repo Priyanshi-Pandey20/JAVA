013c848d98e83225e2c344e32a7bce5261d1f3a6
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-08-04 21:07:08 +0530

    hi

--- Patterns.java
+++ Patterns.java
@@ -51,7 +51,7 @@ public class Patterns {
 
         }
 
-        for (int i = 1; i <= 5; i++) { // hollow square 
+        for (int i = 1; i <= 5; i++) { // hollow square pblm
             for (int j = 1; j <= 5; j++) {
                 if (i == 1 || i == 5 || j == 1 || j == 5) {
                     System.out.print("*");
