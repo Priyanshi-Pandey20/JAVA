740946d533dba9414aa404ede8be3e1431b65777
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-09-04 12:09:40 +0530

    practice

--- Main3.java
+++ Main3.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 abstract class Payment{
    abstract void pay();
 }
@@ -89,20 +91,197 @@ class Child extends Parent{
     
 }
 
+class Address{
+    void display(){
+        System.out.println("Address...");
+    }
+}
+
+// class Student{
+//     Address a = new Address();
+//     void print(){
+//         a.display();
+//         System.out.println("Student address");
+//     }
+// }
+
+class Product{
+    String name;
+
+    Product(String name){
+        this.name = name;
+    }
+}
+
+class Order{
+    List<Product>products = new ArrayList<>();
+
+    void addProduct(Product product){
+        products.add(product);
+    }
+
+    void display(){
+        for(Product p :products){
+            System.out.println(p.name);
+        }
+    }
+}
+
+// class Student {
+//     String name;
+//     int marks;
+
+//     Student(String name,int marks){
+//         this.name = name;
+//         this.marks = marks;
+//     }
+// }
+
+
+// class Employee{
+//     String name;
+//     int id;
+
+//     Employee(String name, int id){
+//         this.id = id;
+//         this.name = name;
+//     }
+// // }
+
+// class Employee{
+//     String name;
+//     int id;
+
+//     Employee(int id, String name){
+//         this.id = id;
+//         this.name = name;
+//     }
+// }
+
+class Student{
+    String name;
+    int id;
+
+    Student(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        Student s = (Student) obj;
+        return this.id == s.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return id;
+    }
+
+    void display(){
+        System.out.println(id + " " + name);
+    }
+
+}
+
+class Employee{
+    int salary;
+
+    Employee(int salary){
+        this.salary = salary;
+    }
+
+}
+
+
+
+
 
 public class Main3 {
     public static void main(String[] agrs){
  
-   Aeroplane a = new Aeroplane();
-   a.fly();
+    // ArrayList<Student> student = new ArrayList<>();
 
-   Bird b  = new Bird();
-   b.fly();
+    // student.add(new Student("Priya", 90));
+    // student.add(new Student("Rahul", 68));
+    // student.add(new Student("Govind", 87));
 
-   Child c = new Child("Priya");
-   c.display();
+    // Student hightest = student.get(0);
+
+    // for (Student s : student){
+    //     if(s.marks > hightest.marks){
+    //         hightest = s;
+    //     }
+    // }
+
+    // System.out.println(hightest.marks);
+    // System.out.println(hightest.name);
+
+    //  Order o = new Order();
+    // o.addProduct(new Product("Laptop"));
+    // o.addProduct(new Product("TV"));
+    // o.addProduct(new Product("Washing Machine"));
+
+    // o.display();
+
+
+    // ArrayList<Employee> emp = new ArrayList<>();
+    // emp.add(new Employee("Priya", 102));
+    // emp.add(new Employee("Rohan", 103));
+    // emp.add(new Employee("Diya", 104));
+
+    // int searchId = 102;
+
+    // for(Employee e : emp){
+    //     if(e.id == searchId){
+    //         System.out.println("Search id : "+ e.id);
+    //         System.out.println("Search name : " + e.name);
+
+
+    //     }
+    // // }
+    // HashMap<Integer,Employee> map = new HashMap<>();
+    // map.put(101,new Employee(101,"Priya"));
+    // map.put(102,new Employee( 102,"Priyanshi"));
+
+    // Employee e = map.get(101);
+    // System.out.println(e.name);
+
+    HashSet<Student> s = new HashSet<>();
+    s.add(new Student(101, "Priya"));
+    s.add(new Student(102, "Rohan"));
+    s.add(new Student(103, "Diya"));
+    s.add(new Student(102, "Seema"));
+
+    for(Student student : s){
+        student.display();
+    }
+
+    ArrayList<Employee> emp = new ArrayList<>();
+   emp.add(new Employee(70000));
+   emp.add(new Employee(42000));
+   emp.add(new Employee(790000));
+   Collections.sort(emp,(e1,e2)-> Integer.compare(e1.salary, e2.salary));
+
+   for(Employee e : emp){
+    System.out.println(e.salary);
+   }
+
+    
+
+    
+
+
+
+    
+
+    
+
+
 
   
+
+
    
 
 
